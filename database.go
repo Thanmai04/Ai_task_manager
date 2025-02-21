11e238991742cbf4bcc512c360cd27216e525808
Thanmai04 <159177700+Thanmai04@users.noreply.github.com>
2025-02-21 10:13:17 +0530

    Add files via upload

---
+++ database.go
@@ -0,0 +1,29 @@
+package db
+
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func ConnectDatabase() {
+	// Load environment variables
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	dsn := os.Getenv("DATABASE_URL")
+	database, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Failed to connect to database:", err)
+	}
+
+	DB = database
+	log.Println("Database connected successfully!")
+}
